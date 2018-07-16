02eab929b01fcbd8e61675d317f4a737e43b5dd6
Jim Chuang <jimchuang.nhr@gmail.com>
2018-07-16 12:31:25 +0800

    unit number compete

--- LeetTest/UnitViewController.swift
+++ LeetTest/UnitViewController.swift
@@ -16,72 +16,119 @@ class eq{
 
 class UnitViewController: UIViewController {
 
+    var totalEQArr = Array(repeating: Array(repeating: eq(), count: 9), count: 9)
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         var myArr = getArr()
-        var eqArr = Array(repeating: Array(repeating: eq(), count: 9), count: 9)
+        //var eqArr = Array(repeating: Array(repeating: eq(), count: 9), count: 9)
         for row in 0...8{
             for colum in 0...8{
                 if myArr[row][colum] == 0{
                     var myEQ = eq()
                     myEQ = setAllToEqArr(allEQ: myEQ)
-                    eqArr[row][colum] = myEQ
+                    totalEQArr[row][colum] = myEQ
                 }else{
                     var myEQ = eq()
                     myEQ = setNumToEqArr(oriEQ: myEQ, num: myArr[row][colum])
-                    eqArr[row][colum] = myEQ
+                    totalEQArr[row][colum] = myEQ
                 }
             }
         }
 
+        let showRow = 2
+        let showColum = 5
         // 1st
-        while  isCompete(oriEQArrArr: eqArr) == false {
+        while  isCompete(oriEQArrArr: totalEQArr) == false {
             for row in 0...8{
                 for colum in 0...8{
-
-                    //row
-                    let subRow = eqArr[row]
-                    for i in 0...subRow.count-1
+                    totalEQArr = minumumEQArr(eqArrArr: totalEQArr)
+                    if isMinimumEQ(oriEq: totalEQArr[row][colum]) == false
                     {
-                        if isMinimumEQ(oriEq: subRow[i])
-                        {
-                            eqArr[row][colum].row = eqArr[row][colum].row.filter{$0 != subRow[i].row[0]}
+                        if row == showRow && colum == showColum{
+                            let tempTest = totalEQArr[row]
+                            DLog(message: "row \(tempTest[showColum].row) colum \(tempTest[showColum].colum) eq \(tempTest[showColum].eq)")
                         }
-                    }
 
-                    //colum
-                    var subColum = Array<eq>()
-                    for i in 0...8
-                    {
-                        subColum.append(eqArr[i][colum])
-                    }
-                    for i in 0...subColum.count-1
-                    {
-                        if isMinimumEQ(oriEq: subColum[i])
-                        {
-                            eqArr[row][colum].colum = eqArr[row][colum].colum.filter{$0 != subColum[i].colum[0]}
+                        //row
+                        do{
+                            let subRow = totalEQArr[row]
+                            for i in 0...subRow.count-1
+                            {
+                                if isMinimumEQ(oriEq: subRow[i]) && i != colum
+                                {
+                                    totalEQArr[row][colum].row = totalEQArr[row][colum].row.filter{$0 != subRow[i].row[0]}
+                                }
+                            }
                         }
-                    }
 
-                    //eq
-                    var subEq = Array<eq>()
-                    subEq = getSubEqArr(oriArr: eqArr, row: row, colum: colum)
-                    for i in 0...subEq.count-1
-                    {
-                        if isMinimumEQ(oriEq: subEq[i])
-                        {
-                            eqArr[row][colum].eq = eqArr[row][colum].eq.filter{$0 != subEq[i].eq[0]}
+                        if row == showRow && colum == showColum{
+                            let tempTest = totalEQArr[row]
+                            DLog(message: "row \(tempTest[showColum].row) colum \(tempTest[showColum].colum) eq \(tempTest[showColum].eq)")
+                        }
+                        if isMinimumEQ(oriEq: totalEQArr[row][colum]){
+                            continue
+                        }
+
+                        //colum
+                        do{
+                            var subColum = Array<eq>()
+                            for i in 0...8
+                            {
+                                subColum.append(totalEQArr[i][colum])
+                            }
+                            for i in 0...subColum.count-1
+                            {
+                                if isMinimumEQ(oriEq: subColum[i]) && i != row
+                                {
+                                    totalEQArr[row][colum].colum = totalEQArr[row][colum].colum.filter{$0 != subColum[i].colum[0]}
+                                }
+                            }
+                        }
+
+                        if row == showRow && colum == showColum{
+                            let tempTest = totalEQArr[row]
+                            DLog(message: "row \(tempTest[showColum].row) colum \(tempTest[showColum].colum) eq \(tempTest[showColum].eq)")
+                        }
+                        if isMinimumEQ(oriEq: totalEQArr[row][colum]){
+                            continue
+                        }
+/*row colum
+                         00 01 02 345 678
+                         10 11 12
+                         20 21 22
+ */
+                        //eq
+                        do{
+                            var subEq = Array<eq>()
+                            subEq = getSubEqArr(oriArr: totalEQArr, row: row, colum: colum)
+                            for i in 0...subEq.count-1
+                            {
+                                let removeRow = row % 3
+                                let removeColum = colum % 3
+                                let removeNum = removeRow * 3 + removeColum
+                                if isMinimumEQ(oriEq: subEq[i]) && i != removeNum
+                                {
+                                    totalEQArr[row][colum].eq = totalEQArr[row][colum].eq.filter{$0 != subEq[i].eq[0]}
+                                }
+                            }
+                        }
+                        if row == showRow && colum == showColum{
+                            let tempTest = totalEQArr[row]
+                            DLog(message: "row \(tempTest[showColum].row) colum \(tempTest[showColum].colum) eq \(tempTest[showColum].eq)")
+                        }
+                        if isMinimumEQ(oriEq: totalEQArr[row][colum]){
+                            continue
                         }
                     }
                 }
             }
 
-            eqArr = minumumEQArr(eqArrArr: eqArr)
-
+            totalEQArr = minumumEQArr(eqArrArr: totalEQArr)
         }
-        showEQArrToNum(oriEQArrArr: eqArr)
-        DLog(message: isCompete(oriEQArrArr: eqArr))
+        showEQArrToNum(oriEQArrArr: totalEQArr)
+        DLog(message: isCompete(oriEQArrArr: totalEQArr))
     }
 
     func getSubEqArr(oriArr:Array<Array<eq>>,row:Int,colum:Int) -> Array<eq>{
@@ -109,6 +156,10 @@ class UnitViewController: UIViewController {
             columFrom += step
             columEnd += step
             columRange = columFrom...columEnd
+        }
+
+        if rowEnd >= 9 || columEnd >= 9{
+            DLog(message: "sub EQ overflow")
         }
 
         for i in rowRange{
@@ -157,6 +208,10 @@ class UnitViewController: UIViewController {
                 let rowArr = resultArr[row][colum].row
                 let columArr = resultArr[row][colum].colum
                 let eqArr = resultArr[row][colum].eq
+                if rowArr.count == 0 || columArr.count == 0 || eqArr.count == 0{
+                    DLog(message: "interrupt row = \(row) colum = \(colum)")
+                    DLog(message: "rowArr = \(rowArr) columArr = \(columArr) eqArr = \(eqArr)")
+                }
                 let minmumArr = intersectionOfThreeArray(arr1: rowArr, arr2: columArr, arr3: eqArr)
                 resultArr[row][colum].colum = minmumArr
                 resultArr[row][colum].row = minmumArr
@@ -172,6 +227,10 @@ class UnitViewController: UIViewController {
             for colum in 0...8{
                 if oriEQArrArr[row][colum].colum.count == 1{
                     myShowArr[row][colum] = oriEQArrArr[row][colum].colum[0]
+                }else if oriEQArrArr[row][colum].row.count == 1{
+                    myShowArr[row][colum] = oriEQArrArr[row][colum].row[0]
+                }else if oriEQArrArr[row][colum].eq.count == 1{
+                    myShowArr[row][colum] = oriEQArrArr[row][colum].eq[0]
                 }else{
                     myShowArr[row][colum] = 0
                 }
@@ -200,6 +259,10 @@ class UnitViewController: UIViewController {
 
     func intersectionOfThreeArray(arr1:Array<Int>,arr2:Array<Int>,arr3:Array<Int>) -> Array<Int>{
         var sumArr = Array<Int>()
+        if arr1.count == 0 || arr2.count == 0 || arr3.count == 0{
+            DLog(message: "interrupt")
+            DLog(message: showEQArrToNum(oriEQArrArr: totalEQArr))
+        }
         for x in 0...arr1.count-1{
             for y in 0...arr2.count-1{
                 for z in 0...arr3.count-1{
@@ -218,6 +281,18 @@ class UnitViewController: UIViewController {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
     func getArr() -> Array<Array<Int>> {
         let arr = [[5,3,0,0,7,0,0,0,0],
                    [6,0,0,1,9,5,0,0,0],
@@ -228,6 +303,7 @@ class UnitViewController: UIViewController {
                    [0,6,0,0,0,0,2,8,0],
                    [0,0,0,4,1,9,0,0,5],
                    [0,0,0,0,8,0,0,7,9]]
+
        let arr2 = [[5,3,5,5,5,5,5,5,5],
                    [5,3,5,5,5,5,5,5,5],
                    [5,3,5,5,5,5,5,5,5],
@@ -248,6 +324,15 @@ class UnitViewController: UIViewController {
 
 
 /*
+     -UnitViewController.swift showEQArrToNum(oriEQArrArr:)-[189]: [5, 3, 0, 0, 7, 0, 0, 0, 0]
+     -UnitViewController.swift showEQArrToNum(oriEQArrArr:)-[189]: [6, 0, 0, 1, 9, 5, 0, 0, 0]
+     -UnitViewController.swift showEQArrToNum(oriEQArrArr:)-[189]: [0, 9, 8, 0, 0, 0, 0, 6, 0]
+     -UnitViewController.swift showEQArrToNum(oriEQArrArr:)-[189]: [8, 0, 0, 0, 6, 0, 0, 0, 3]
+     -UnitViewController.swift showEQArrToNum(oriEQArrArr:)-[189]: [4, 0, 0, 8, 5, 3, 0, 0, 1]
+     -UnitViewController.swift showEQArrToNum(oriEQArrArr:)-[189]: [7, 0, 0, 0, 2, 0, 0, 0, 6]
+     -UnitViewController.swift showEQArrToNum(oriEQArrArr:)-[189]: [0, 6, 0, 0, 0, 7, 2, 8, 4]
+     -UnitViewController.swift showEQArrToNum(oriEQArrArr:)-[189]: [3, 0, 0, 4, 1, 9, 6, 3, 5]
+     -UnitViewController.swift showEQArrToNum(oriEQArrArr:)-[189]: [0, 0, 0, 0, 8, 0, 0, 7, 9]
      530070000
      600195000
      098000060
@@ -260,3 +345,4 @@ class UnitViewController: UIViewController {
 
  */
 }
+
